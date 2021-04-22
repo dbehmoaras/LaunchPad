cebf71bfced4a173731797373b9d18fed006c263
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-04-22 15:09:49 -0400

    converted login query from promise to callback

--- server/controllers/userAuthControl/userController.js
+++ server/controllers/userAuthControl/userController.js
@@ -7,7 +7,7 @@ userController.signUp = (req, res, next) => {
 
   console.log('userController.signUp:',req.body)
 
-  const values = [
+  const qValues = [
     req.body.email,
     req.body.password,
     req.body.lastName,
@@ -18,7 +18,7 @@ userController.signUp = (req, res, next) => {
     `INSERT INTO users_crypt (email, hash, lastname, firstname)
     VALUES ($1, crypt($2 ,gen_salt('bf')), $3, $4)`;
 
-  db.query(qString, values)
+  db.query(qString, qValues)
     .then(data => {
       console.log('userController.signUp USER ADDED:',data);
       res.locals.newUser = data.rows[0];
@@ -35,24 +35,37 @@ userController.logIn = (req, res, next) => {
   console.log('userController.logIn', req.body);
 
 
-  const values = [
+  const qValues = [
     req.body.email,
-    req.body.password,
   ]
+  console.log(qValues)
   const qString = `SELECT email, hash FROM users_crypt WHERE email = $1`;
 
+  db.query(qString, qValues, (err, data) => {
+    if (err) {
+      next({
+        log: err,
+        err: 'ERROR: userController.logIn failed to query a user in the database'
+      })
+    }
+    console.log('THIS IS NOW A CALLBACK',data);
+    res.json(data)
+    next();
+  })
 
-
-  db.query(qString, values)
-    .then(data => {
-      console.log('NEXT STEP: invoke next, move to cookie middleWare. data:',data.rows[0]);
-      res.json(data.rows[0]);
-    })
-    .then(next)
-    .catch(err => next({
-      log: err,
-      err: 'ERROR: userController.logIn failed to query a user in the database'
-    }))
+  /* promise chain form of query
+   db.query(qString, values)
+     .then(data => {
+       //compare the passwords then 2 different responses
+       console.log('NEXT STEP: invoke next, move to cookie middleWare. data:',data.rows[0]);
+       res.json(data.rows[0]);
+     })
+     .then(next)
+     .catch(err => next({
+       log: err,
+       err: 'ERROR: userController.logIn failed to query a user in the database'
+     }))
+  */
 }
 
 
